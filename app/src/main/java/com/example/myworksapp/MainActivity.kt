6ef6b9d45b9697f389dbaf9786220edc4b5c6920
Harshani R Liyanage <it22578464@my.sliit.lk>
2024-05-12 14:39:31 +0530

    2nd commit

--- app/src/main/java/com/example/myworksapp/MainActivity.kt
+++ app/src/main/java/com/example/myworksapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myworksapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent // Add import statement for Intent
+import com.example.myworksapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
