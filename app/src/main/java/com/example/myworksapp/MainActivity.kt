ed34b3ba871f1a6f52dfdd06fe7df6e814820fd5
Harshani R Liyanage <it22578464@my.sliit.lk>
2024-05-12 18:11:24 +0530

    read data

--- app/src/main/java/com/example/myworksapp/MainActivity.kt
+++ app/src/main/java/com/example/myworksapp/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.example.myworksapp
 
+import NoteDatabaseHelper
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.Intent // Add import statement for Intent
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myworksapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NoteDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = NoteDatabaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes())
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
         binding.addButton.setOnClickListener {
             val intent = Intent(this@MainActivity, AddNoteActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
     }
 }
